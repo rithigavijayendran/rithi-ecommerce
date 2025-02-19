a02815b864ef153986db9fbf7bf1a88325d17733
Rithiga V <138615122+rithigavijayendran@users.noreply.github.com>
2025-02-19 18:19:00 +0530

    first commit

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://rithi-ecommerce-backend.onrender.com";
+export const BASE_URL = "https://rithi-ecommercebackend.onrender.com";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
